dcb8746cc13780a803dfb0c84051be389d25e1ff
Your Name <you@example.com>
2024-12-05 21:33:53 +0300

    Adjusted indentation and some corrections

--- recursive_binary_search02.py
+++ recursive_binary_search02.py
@@ -3,25 +3,25 @@ def recursive_binary_search(list, target, left, right):
   if left > right:
     return False
   
-    # Calculate the middle point
-    midpoint = (left + right) // 2
+  # Calculate the middle point
+  midpoint = (left + right) // 2
 
-    # check if midpoint is the target
-    if midpoint == target:
-      return midpoint
-    # If target is less than midpoint, search the lower half
-    elif list[midpoint] > target:
-      return recursive_binary_search(list, target, left, midpoint - 1)
-    else:
-      return recursive_binary_search(list, target, midpoint + 1, right)
+  # check if midpoint is the target
+  if list[midpoint] == target:
+    return midpoint
+  # If target is less than midpoint, search the lower half
+  elif list[midpoint] > target:
+    return recursive_binary_search(list, target, left, midpoint - 1)
+  else:
+    return recursive_binary_search(list, target, midpoint + 1, right)
     
 
 list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 target = 7
-result = recursive_binary_search(list, target, 0, len(list))
+result = recursive_binary_search(list, target, 0, len(list) - 1)
 
 
-if result != False:
+if result is not False:
     print(f"Element found at index {result}")
 else:
     print("Element not found")
